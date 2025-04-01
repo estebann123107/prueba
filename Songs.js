63cfbe221930b9565553f904032caaca46ba16e7
conan <estebanmelendez0902@gmail.com>
2025-04-01 17:07:36 -0500

    .

---
+++ Songs.js
@@ -0,0 +1,368 @@
+const list = {
+    Sunsetseason :{
+        tittle: "Sunset Season",
+
+        first:"1. Idle Town",
+        link1:"Lyrics.html?Song=IdleTown",
+        mostrarCancion1: true,
+
+        second:"2. Generation Why",
+        link2:"Lyrics.html?Song=GenerationWhy",
+        mostrarCancion2: true,
+
+        third:"3. Crush Culture",
+        link3:"Lyrics.html?Song=CrushCulture",
+        mostrarCancion3: true,
+
+        four:"4. Greek God",
+        link4:"Lyrics.html?Song=GreekGod",
+        mostrarCancion4: true,
+
+        five: "5. Lookalike",
+        link5:"Lyrics.html?Song=Lookalike",
+        mostrarCancion5: true,
+
+        six: "askkash",
+        mostrarCancion6: false,
+        link6:"Lyrics.html?Song=",
+
+        seven: "",
+        mostrarCancion7: false,
+        link7:"Lyrics.html?Song=",
+
+        eight: "",
+        mostrarCancion8: false,
+        link8:"Lyrics.html?Song=",
+
+        nine: "",
+        mostrarCancion9: false,
+        link9:"Lyrics.html?Song=",
+
+        ten: "",
+        mostrarCancion10: false,
+        link10:"Lyrics.html?Song=",
+
+        eleven: "",
+        mostrarCancion11: false,
+        link11:"Lyrics.html?Song=",
+
+        twelve: "",
+        mostrarCancion12: false,
+        link12:"Lyrics.html?Song=",
+
+        thirteen: "",
+        mostrarCancion13: false,
+        link13:"Lyrics.html?Song=",  
+    },
+    KidKrow :{
+        tittle: "Kid Krow",
+
+        first:"1. Confort Crowd",
+        link1:"Lyrics.html?Song=ConfortCrowd",
+        mostrarCancion1: true,
+
+        second:"2. Wish You Were Sober",
+        link2:"Lyrics.html?Song=WishYouWereSober",
+        mostrarCancion2: true,
+
+        third:"3. Maniac",
+        link3:"Lyrics.html?Song=Maniac",
+        mostrarCancion3: true,
+
+        four:"4. (Online Love)",
+        link4:"Lyrics.html?Song=OnlineLove",
+        mostrarCancion4: true,
+
+        five: "5. Checkmate",
+        link5:"Lyrics.html?Song=Checkmate",
+        mostrarCancion5: true,
+
+        six: "6. The Cut That Always Bleeds",
+        mostrarCancion6: true,
+        link6:"Lyrics.html?Song=TheCutThatAlwaysBleeds",
+
+        seven: "7. Fight or Flight",
+        mostrarCancion7: true,
+        link7:"Lyrics.html?Song=FightOrFlight",
+
+        eight: "8. Affluenza",
+        mostrarCancion8: true,
+        link8:"Lyrics.html?Song=Affluenza",
+
+        nine: "9. (Can We be Friends)",
+        mostrarCancion9: true,
+        link9:"Lyrics.html?Song=CanWeBeFriends",
+
+        ten: "10. Heather",
+        mostrarCancion10: true,
+        link10:"Lyrics.html?Song=Heather",
+
+        eleven: "11. Little League",
+        mostrarCancion11: true,
+        link11:"Lyrics.html?Song=LittleLeague",
+
+        twelve: "12. The Story",
+        mostrarCancion12: true,
+        link12:"Lyrics.html?Song=TheStory",
+
+        thirteen: "13. Bed Rest",
+        mostrarCancion13: true,
+        link13:"Lyrics.html?Song=BedRest",  
+    },
+    Superache :{
+        tittle: "Superache",
+
+        first:"1. Movies",
+        link1:"Lyrics.html?Song=Movies",
+        mostrarCancion1: true,
+
+        second:"2. People Watching",
+        link2:"Lyrics.html?Song=PeopleWatching",
+        mostrarCancion2: true,
+
+        third:"3. Disaster",
+        link3:"Lyrics.html?Song=Disaster",
+        mostrarCancion3: true,
+
+        four:"4. Best Friend",
+        link4:"Lyrics.html?Song=BestFriend",
+        mostrarCancion4: true,
+
+        five: "5. Astronomy",
+        link5:"Lyrics.html?Song=Astronomy",
+        mostrarCancion5: true,
+
+        six: "6. Yours",
+        mostrarCancion6: true,
+        link6:"Lyrics.html?Song=Yours",
+
+        seven: "7. Jigsaw",
+        mostrarCancion7: true,
+        link7:"Lyrics.html?Song=Jigsaw",
+
+        eight: "8. Family Line",
+        mostrarCancion8: true,
+        link8:"Lyrics.html?Song=FamilyLine",
+
+        nine: "9. Summer Child",
+        mostrarCancion9: true,
+        link9:"Lyrics.html?Song=SummerChild",
+
+        ten: "10. Footnote",
+        mostrarCancion10: true,
+        link10:"Lyrics.html?Song=Footnote",
+
+        eleven: "11. Memories",
+        mostrarCancion11: true,
+        link11:"Lyrics.html?Song=Memories",
+
+        twelve: "12. The Exit",
+        mostrarCancion12: true,
+        link12:"Lyrics.html?Song=TheExit",
+
+        thirteen: "13. Bed Rest",
+        mostrarCancion13: false,
+        link13:"Lyrics.html?Song=BedRest",  
+    },
+    FoundHeaven :{
+        tittle: "Found Heaven",
+
+        first:"1. Found Heaven",
+        link1:"Lyrics.html?Song=FoundHeaven",
+        mostrarCancion1: true,
+
+        second:"2. Never Ending Song",
+        link2:"Lyrics.html?Song=NeverEndingSong",
+        mostrarCancion2: true,
+
+        third:"3. ",
+        link3:"Lyrics.html?Song=",
+        mostrarCancion3: true,
+
+        four:"4. ",
+        link4:"Lyrics.html?Song=",
+        mostrarCancion4: true,
+
+        five: "5. ",
+        link5:"Lyrics.html?Song=",
+        mostrarCancion5: true,
+
+        six: "6. ",
+        mostrarCancion6: true,
+        link6:"Lyrics.html?Song=",
+
+        seven: "7. ",
+        mostrarCancion7: true,
+        link7:"Lyrics.html?Song=",
+
+        eight: "8. ",
+        mostrarCancion8: true,
+        link8:"Lyrics.html?Song=",
+
+        nine: "9. ",
+        mostrarCancion9: true,
+        link9:"Lyrics.html?Song=",
+
+        ten: "10. ",
+        mostrarCancion10: true,
+        link10:"Lyrics.html?Song=",
+
+        eleven: "11. ",
+        mostrarCancion11: true,
+        link11:"Lyrics.html?Song=",
+
+        twelve: "12. ",
+        mostrarCancion12: true,
+        link12:"Lyrics.html?Song=",
+
+        thirteen: "13. ",
+        mostrarCancion13: true,
+        link13:"Lyrics.html?Song=",  
+    },
+};
+function obtenerParametro(nombre){
+    const params = new
+    URLSearchParams(window.location.search);
+    return params.get(nombre)
+}
+
+const albumKey = obtenerParametro("album");
+
+//link para letras
+if(list[albumKey]){
+    const firstElem =document.getElementById("first");
+    firstElem.innerText = list[albumKey].first;
+    firstElem.href = list[albumKey].link1;
+}
+if(list[albumKey]){
+    const secondElem =document.getElementById("second");
+    secondElem.innerText = list[albumKey].second;
+    secondElem.href = list[albumKey].link2;
+}
+if(list[albumKey]){
+    const thirdElem =document.getElementById("third");
+    thirdElem.innerText = list[albumKey].third;
+    thirdElem.href = list[albumKey].link3;
+}
+if(list[albumKey]){
+    const fourElem =document.getElementById("four");
+    fourElem.innerText = list[albumKey].four;
+    fourElem.href = list[albumKey].link4;
+}
+if(list[albumKey]){
+    const fiveElem =document.getElementById("five");
+    fiveElem.innerText = list[albumKey].five;
+    fiveElem.href = list[albumKey].link5;
+}
+if(list[albumKey]){
+    const sixElem =document.getElementById("six");
+    sixElem.innerText = list[albumKey].six;
+    sixElem.href = list[albumKey].link6;
+}
+if(list[albumKey]){
+    const sevenElem =document.getElementById("seven");
+    sevenElem.innerText = list[albumKey].seven;
+    sevenElem.href = list[albumKey].link7;
+}
+if(list[albumKey]){
+    const eightElem =document.getElementById("eight");
+    eightElem.innerText = list[albumKey].eight;
+    eightElem.href = list[albumKey].link8;
+}
+if(list[albumKey]){
+    const nineElem =document.getElementById("nine");
+    nineElem.innerText = list[albumKey].nine;
+    nineElem.href = list[albumKey].link9;
+}
+if(list[albumKey]){
+    const tenElem =document.getElementById("ten");
+    tenElem.innerText = list[albumKey].ten;
+    tenElem.href = list[albumKey].link10;
+}
+if(list[albumKey]){
+    const elevenElem =document.getElementById("eleven");
+    elevenElem.innerText = list[albumKey].eleven;
+    elevenElem.href = list[albumKey].link11;
+}
+if(list[albumKey]){
+    const twelveElem =document.getElementById("twelve");
+    twelveElem.innerText = list[albumKey].twelve;
+    twelveElem.href = list[albumKey].link12;
+}
+if(list[albumKey]){
+    const thirteenElem =document.getElementById("thirteen");
+    thirteenElem.innerText = list[albumKey].thirteen;
+    thirteenElem.href = list[albumKey].link13;
+}
+
+//titulo album
+if (list[albumKey]){
+    document.getElementById("tittle").innerText = list[albumKey].tittle}
+
+//mostrar o ocultar
+if (list[albumKey].mostrarCancion1){
+    document.getElementById("first").innerText = list[albumKey].first
+} else{
+    document.getElementById("first").style.display = "none";
+}
+if (list[albumKey].mostrarCancion2){
+    document.getElementById("second").innerText = list[albumKey].second
+} else{
+    document.getElementById("second").style.display = "none";
+}
+if (list[albumKey].mostrarCancion3){
+    document.getElementById("third").innerText = list[albumKey].third
+} else{
+    document.getElementById("third").style.display = "none";
+}
+if (list[albumKey].mostrarCancion4){
+    document.getElementById("four").innerText = list[albumKey].four
+} else{
+    document.getElementById("four").style.display = "none";
+}
+if (list[albumKey].mostrarCancion5){
+    document.getElementById("five").innerText = list[albumKey].five
+} else{
+    document.getElementById("five").style.display = "none";
+}
+if (list[albumKey].mostrarCancion6){
+    document.getElementById("six").innerText = list[albumKey].six
+} else{
+    document.getElementById("six").style.display = "none";
+}
+if (list[albumKey].mostrarCancion7){
+    document.getElementById("seven").innerText = list[albumKey].seven
+} else{
+    document.getElementById("seven").style.display = "none";
+}
+if (list[albumKey].mostrarCancion8){
+    document.getElementById("eight").innerText = list[albumKey].eight
+} else{
+    document.getElementById("eight").style.display = "none";
+}
+if (list[albumKey].mostrarCancion9){
+    document.getElementById("nine").innerText = list[albumKey].nine
+} else{
+    document.getElementById("nine").style.display = "none";
+}
+if (list[albumKey].mostrarCancion10){
+    document.getElementById("ten").innerText = list[albumKey].ten
+} else{
+    document.getElementById("ten").style.display = "none";
+}
+if (list[albumKey].mostrarCancion11){
+    document.getElementById("eleven").innerText = list[albumKey].eleven
+} else{
+    document.getElementById("eleven").style.display = "none";
+}
+if (list[albumKey].mostrarCancion12){
+    document.getElementById("twelve").innerText = list[albumKey].twelve
+} else{
+    document.getElementById("twelve").style.display = "none";
+}
+if (list[albumKey].mostrarCancion13){
+    document.getElementById("thirteen").innerText = list[albumKey].thirteen
+} else{
+    document.getElementById("thirteen").style.display = "none";
+}
+
---
+++ main.js
@@ -0,0 +1,47 @@
+const template = document.getElementById('cover-template')
+
+const Albums = [
+    {
+        title: 'Sunset Season',
+        link: 'Songs.html?album=Sunsetseason',
+        image: 'Images/SunsetSeason.jpg',
+        class: 'sun'
+
+    },
+    {
+        title: 'Kid Krow',
+        link: 'Songs.html?album=KidKrow',
+        image: 'Images/KitKrow.jpeg',
+        class: 'kid'
+    },
+    {
+        title: 'Superache',
+        link: 'Songs.html?album=Superache',
+        image: 'Images/Superache.jpeg',
+        class: 'sup'
+    },
+    {
+        title: 'Found Heaven',
+        link: 'Songs.html?album=FoundHeaven',
+        image: 'Images/FoundHeaven.jpeg',
+        class: 'fh'
+    },
+]
+
+const albumsGroup = document.querySelector('.Albumsgroup')
+
+
+
+Albums.forEach(album => {
+    const albumTemplete = template.content.cloneNode(true).querySelector('.Cover')
+    const link = albumTemplete.querySelector('a')
+    link.setAttribute('href', album.link)
+    const title = albumTemplete.querySelector('h3')
+    title.innerText = album.title
+    title.classList.add(album.class)
+    const image = albumTemplete.querySelector('img')
+    image.setAttribute('src', album.image)
+
+    albumsGroup.appendChild(albumTemplete)
+})
+ 
