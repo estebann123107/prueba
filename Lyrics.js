1a061774618d0f9670572c7ed1fc55800b556d6b
conan <estebanmelendez0902@gmail.com>
2025-04-02 17:01:22 -0500

    new

--- Lyrics.js
+++ Lyrics.js
@@ -1,6 +1,7 @@
 const list ={
     IdleTown:{
         tittle:"Idle Town",
+        back:"Songs.html?album=Sunsetseason",
         textColor: "rgb(245, 106, 81)",
         fondo:"/Images/SunsetSeason.jpg",
         letra:"This town will never change\nPeople come and go, it's all the same\nSpeed the roads on our doubting days\nTo any place that's far away\n\nI never learned anybody's name\nWe all vowed that we wouldn't stay\nKissing signs on the interstate\nAll we do for now is sit and wait\n\nIn our idle town, in our idle town\n\nYeah, we invent our own little games\nWhen the lights out at the stadium, hey\nMaking life a spinning arcade\nAnd curfew's at midnight, we watch the sunrise\nWe watch the sunrise\nWe watch the Sun\n\nSpringtime when the air is thin\nWe're getting antsy and they're noticing\nWhile everybody's off partying\nWe're watching stars over ice-cream\n\nAnd now we're kicking up to seventh gear\nAnd we're breathing in the atmosphere\nAnd I don't even need a sip of beer\nYeah I can feel it all\n\nIn our idle town, in our idle town\n\nYeah, we invent our own little games\nWhen the lights out at the stadium, hey\nMaking life a spinning arcade\nAnd curfew's at midnight, we watch the sunrise\nWe watch the sunrise\nWe watch the Sun\n\nAnd they watched the sunrise\nBlinking out of red eyes, and sore minds\nThe airplanes keep flying by, and they cry\n'Cause they've never even touched the sky\nNo, they'll never even touch the sky\n\nYeah we invent our own little games\nAnd everybody's trying to escape\nKilling time at the yellow café\nAnd curfew's at midnight\n\nYeah I think that we'll all be okay\nThe idle town will stay as it stays\nAnd everyone I love's in this place\nSo curfew's at midnight\nWe watch the sunrise",
@@ -10,6 +11,7 @@ const list ={
     },
     GenerationWhy:{
         tittle:"Generation Why",
+        back:"Songs.html?album=Sunsetseason",
         textColor: "rgb(245, 106, 81)",
         fondo:"/Images/SunsetSeason.jpg",
         letra:"Why, why, why, why\nWhy, why, why, why\nWhy, why, why\n\nI was off\nKeying cars parked on radium lawns\nBy suburbian moms\nI called a friend\nLet's meet at ten\nGo wherever we want\n'Cause no one cares that we're gone\n\nThis town don't got much to do\nYou and I haven't got much to lose\nSo do you wanna rot in your room like we always do?\nTalk about how fast we grew\nAnd all the big dreams that we won't pursue\nThen get in your car and laugh till we both turn blue\n\n'Cause we are the helpless, selfish, one of a kind\nMillennium kids, that all wanna die\nWalking in the street with no light inside our eyes\n(Why)\nWe are the worthless, cursed with too much time\nWe get into trouble and lose our minds\nSomething that I've heard a million times in my life\nGeneration Why\n\nParents think, we're fast asleep\nBut as soon as we're home, we're sneaking out the window\n'Cause at this rate of earth decay\nOur world's ending at noon\nCan we all just move to the Moon?\n\nThis town don't got much to do\nAnd you and I haven't got much to lose\nSo do you wanna leave everyone in this place for good?\n\n'Cause we are the helpless, selfish, one of a kind\nMillennium kids, that all wanna die\nWalking in the street with no light inside our eyes\nWe are the worthless, cursed with too much time\nWe get into trouble and lose our minds\nSomething that I've heard a million times in my life\nGeneration Why\n\nWhy, why, why, why, why, why, why, why\nWe're livin' night to night\nWhy, why, why, why, why, why, why, why\nSince we're bound to die, oh\nWhy, why, why, why, why, why, why, why\nOh, what's the use in trying?\nWhy, why, why, why, why, why, why, why\nAnd it's exactly why\n\n'Cause we are the helpless, selfish, one of a kind\nMillennium kids, that all wanna die\nWalking in the street with no light inside our eyes\nWe are the worthless, cursed with too much time\nWe get into trouble and lose our minds\nSomething that I've heard a million times in my life\nGeneration why\n\nWhy, why, why, why, why, why, why, why?\nWhy, why, why, why, why, why, why, why?\nWhy, why, why, why, why, why, why, why?\nWhy, why, why, why, why, why, why, why?",
@@ -19,6 +21,7 @@ const list ={
     },
     CrushCulture:{
         tittle:"Crush Culture",
+        back:"Songs.html?album=Sunsetseason",
         textColor: "rgb(245, 106, 81)",
         fondo:"/Images/SunsetSeason.jpg",
         letra:"",
@@ -53,6 +56,12 @@ if(list[songKey]){
     document.documentElement.style.setProperty("--fondo", `url(${list[songKey].fondo})`);
 }
 
+
+//back
+if (list[songKey]){
+    document.getElementById("back").href = list[songKey].back}
+
+    
 //titulo
 if (list[songKey]){
     document.getElementById("tittle").innerText = list[songKey].tittle}
