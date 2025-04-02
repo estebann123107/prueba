be9646b1216713abeaf26acab69ce71c72b00326
conan <estebanmelendez0902@gmail.com>
2025-04-02 14:52:16 -0500

    new

--- Lyrics.js
+++ Lyrics.js
@@ -3,6 +3,8 @@ const list ={
         tittle:"Idle Town",
         textColor: "rgb(245, 106, 81)",
         fondo:"/Images/SunsetSeason.jpg",
+        letra:"letra",
+        traduccion:"traduccion",
     }
 }
 
@@ -13,12 +15,23 @@ function obtenerParametro(nombre){
 }
 
 const songKey = obtenerParametro("Song");
-
+//tema
 if(list[songKey]){
     document.documentElement.style.setProperty("--colorTexto",list[songKey].textColor);
 }
 if(list[songKey]){
     document.documentElement.style.setProperty("--fondo", `url(${list[songKey].fondo})`);
 }
+
+//titulo
 if (list[songKey]){
     document.getElementById("tittle").innerText = list[songKey].tittle}
+    
+
+//letra
+if (list[songKey]){
+        document.getElementById("letra").innerText = list[songKey].letra}
+
+//traduccion
+if (list[songKey]){
+    document.getElementById("traduccion").innerText = list[songKey].traduccion}
