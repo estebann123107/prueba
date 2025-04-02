4950da333f92e36fe93bb2908da51285d3264ba8
conan <estebanmelendez0902@gmail.com>
2025-04-02 15:50:12 -0500

    new

--- Lyrics.js
+++ Lyrics.js
@@ -6,7 +6,7 @@ const list ={
         letra:"This town will never change\nPeople come and go, it's all the same\nSpeed the roads on our doubting days\nTo any place that's far away\n\n\nI never learned anybody's name\nWe all vowed that we wouldn't stay\nKissing signs on the interstate\nAll we do for now is sit and wait\n\n\nIn our idle town, in our idle town\n\n\nYeah, we invent our own little games\nWhen the lights out at the stadium, hey\nMaking life a spinning arcade\nAnd curfew's at midnight, we watch the sunrise\nWe watch the sunrise\nWe watch the Sun\n\n\nSpringtime when the air is thin\nWe're getting antsy and they're noticing\nWhile everybody's off partying\nWe're watching stars over ice-cream\n\n\nAnd now we're kicking up to seventh gear\nAnd we're breathing in the atmosphere\nAnd I don't even need a sip of beer\nYeah I can feel it all\n\n\nIn our idle town, in our idle town\n\n\nYeah, we invent our own little games\nWhen the lights out at the stadium, hey\nMaking life a spinning arcade\nAnd curfew's at midnight, we watch the sunrise\nWe watch the sunrise\nWe watch the Sun\n\n\nAnd they watched the sunrise\nBlinking out of red eyes, and sore minds\nThe airplanes keep flying by, and they cry\n'Cause they've never even touched the sky\nNo, they'll never even touch the sky\n\n\nYeah we invent our own little games\nAnd everybody's trying to escape\nKilling time at the yellow café\nAnd curfew's at midnight\n\n\nYeah I think that we'll all be okay\nThe idle town will stay as it stays\nAnd everyone I love's in this place\nSo curfew's at midnight\nWe watch the sunrise",
         traduccion:"traduccion",
         video:"https://www.youtube.com/embed/BI5_hpUxDrM?si=m_Tk3-jIMy_KfFTD",
-        reproductor:""
+        reproductor:"/Audios/IdleTown.mp3"
     }
 }
 
@@ -44,4 +44,4 @@ if (list[songKey]){
 
 //audio
 if (list[songKey]){
-    document.querySelector('audio').source.src = list[songKey].reproductor}
+    document.querySelector('source').src = list[songKey].reproductor}
